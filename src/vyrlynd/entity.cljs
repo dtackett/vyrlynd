898ae979d07712a787aeb8c746f9a40ae3516148
Devon Tackett <frogplague@gmail.com>
2014-04-21 21:58:41 -0700

    Added function to update component on an entity.

--- src/vyrlynd/entity.cljs
+++ src/vyrlynd/entity.cljs
@@ -10,8 +10,18 @@
   [entity component]
   (assoc entity (components/component-name component) component))
 
-(defn compose-entity [components]
+;; TOTHINK Should this be expanded allow multiple component updates at a time?
+(defn update-component
+  "Update a component"
+  [entity component-name component-data]
+  (add-component entity (merge (get entity component-name) component-data)))
+
+(defn compose-entity
+  "Create a new entity from a series of components"
+  [components]
   (reduce add-component {} components))
 
-(defn has-component [entity component]
+(defn has-component
+  "Check if the given entity has the given component"
+  [entity component]
   (contains? entity component))
